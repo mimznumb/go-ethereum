e6074ad6f39306dd98306d4ed80c39e167c0235a
Mariya Gudeva <mariya.grudeva@itgix.com>
2025-11-16 22:40:08 +0200

    add role to eks

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -14,6 +14,24 @@ module "eks" {
   irsa_service_account_name      = "geth-devnet-sa"
   irsa_service_account_namespace = "default"
 
+  manage_aws_auth = true
+
+  aws_auth_roles = [
+    # GitHub Actions role for Helm deploy
+    {
+      rolearn  = aws_iam_role.github_eks_deploy.arn
+      username = "github-eks-deploy"
+      groups   = ["system:masters"]
+    },
+
+    # (optional) your own admin IAM user or role, if not already there
+    # {
+    #   rolearn  = aws_iam_role.some_admin.arn
+    #   username = "admin"
+    #   groups   = ["system:masters"]
+    # },
+  ]
+
   tags = {
     Environment = "dev"
   }
